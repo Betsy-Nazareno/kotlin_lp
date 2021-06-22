64513dadd98b324694eaed2968da0f02a6869b84
Betsii77 <belinaza@espol.edu.ec>
2021-06-21 23:36:56 -0500

    g

--- kotlin_lexico.py
+++ kotlin_lexico.py
@@ -221,13 +221,12 @@ def t_ID(t):
 # Regla de expresión regular para decimales
 def t_FLOAT(t):
     r'-?[0-9]+\.[0-9]+'
-    t.value = float(t.value[:-1])
     return t
 
 # Regla de expresión regular para Long
 def t_LONG(t):
-    r'(-[1-9][0-9]*)|([0-9]+)'
-    t.value = int(t.value[:-1])
+    r'(-[1-9][0-9]*)|([0-9]+)L'
+    t.value= int(t.value[:-1])
     return t
 
 # Regla de expresión regular para enteros
@@ -236,7 +235,7 @@ suma = r'(' + entero + t_PLUS + entero + r')'
 
 @TOKEN(entero)
 def t_INT(t):
-    #r'(-[1-9][0-9]*)|(\d+)'
+    r'(-[1-9][0-9]*)|(\d+)'
     t.value = int(t.value)
     return t
 
