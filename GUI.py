73eb15f4660f0dd4b639c9acce1803f0fefe6341
karladuran28 <kaduran1998@gmail.com>
2021-07-14 14:14:35 -0500

    boton limpiar funcionando

--- GUI.py
+++ GUI.py
@@ -35,7 +35,7 @@ window.config(menu=menu)
 
 label = tk.Label(frame,text='Analizador del Lenguaje Kotlin', bd='3',fg='black', font='Helvetica 12 bold').grid(row=0,column=0,columnspan=4)  # placing labels
 
-#checks 
+#checks
 check_button1 = tk.Checkbutton(frame, text="Léxico", variable= lexico, onvalue=1, offvalue=0)
 check_button2= tk.Checkbutton(frame, text="Sintáctico", variable= Sintactico, onvalue=1, offvalue=0)
 check_button3 = tk.Checkbutton(frame, text="Semántico", variable= Semantico, onvalue=1, offvalue=0)
@@ -43,12 +43,16 @@ check_button1.grid(pady=10,row=1,column=1, columnspan=1)
 check_button2.grid(pady=10,row=1,column=2, columnspan=1)
 check_button3.grid(pady=10,row=1,column=3, columnspan=1)
 
+
 #input field
 label1 = tk.Label(frame,text='Input', bd='1',fg='black', font='Helvetica 9 bold').grid(sticky = 'W',pady=10,row=1,column=0, columnspan=1)
 entry_field1= tk.Text(frame,height = 12, width=47,bd='5')
 entry_field1.grid(sticky = 'W', row=2, column=0, columnspan=4)
+entry_field1.config(state='normal')
 analizar = tk.Button(frame, text="Analizar", command=lambda: getValue(entry_field1.get("1.0", "end-1c"))).grid(sticky = 'W', padx=100, pady=10, row=3, column=1,columnspan=4)
-clear_button = tk.Button(frame, text="Limpiar", command='').grid(sticky = 'E',pady=10, padx=3, row=3, column=3,columnspan=1)
+clear_button = tk.Button(frame, text="Limpiar", command=lambda: entry_field1.delete("1.0", "end-1c")).grid(sticky = 'E',pady=10, padx=3, row=3, column=3,columnspan=1)
+
+
 #output field
 label2 = tk.Label(frame,text='Output', bd='1',fg='black', font='Helvetica 9 bold').grid(sticky = 'W',pady=10,row=4, column=0,columnspan=1)
 p1 = tk.StringVar(frame, "Resultado")
