7393845a97ae428ac28cedbb85f5ea4fe16ff9a1
karladuran28 <kaduran1998@gmail.com>
2021-07-14 23:01:11 -0500

    errores tipo de dato

--- kotlin_sintactico.py
+++ kotlin_sintactico.py
@@ -77,8 +77,8 @@ def p_impresion(p):
                 |  PRINTLN LPAREN expression RPAREN'''
 
 def p_impresion_error(p):
-    '''funcion : PRINT LPAREN error RPAREN'''
-    cola.append("Error Semántico en la sentencia print. Expresión no válida.")
+    '''impresion : PRINT LPAREN error RPAREN'''
+    cola.append("Linea " + str(lexer.lineno) + ": Error Semántico en la sentencia print. Expresión no válida.")
 
 def p_queue(p):
     'queue : VAL ID DOTS QUEUE LANGLE tipoDato RANGLE EQUAL LINKEDL LANGLE tipoDato RANGLE LPAREN RPAREN'
@@ -116,13 +116,55 @@ def p_asignacionS(p):
 
 
 def p_tipoDatoEspecifico(p):
-    '''tipoDatoEspecifico : TINT EQUAL INT
-                            | TLONG EQUAL LONG
-                            | TFLOAT EQUAL FLOAT
-                            | TBOOLEAN EQUAL BOOLEAN
-                            | TCHAR EQUAL CHAR
-                            | TSTRING EQUAL STRING_1'''
+    '''tipoDatoEspecifico : tipoEntero
+                            | tipoLong
+                            | tipoFloat
+                            | tipoBoolean
+                            | tipoChar
+                            | tipoString'''
 
+
+def p_tipoEntero(p):
+    'tipoEntero : TINT EQUAL INT'
+
+def p_tipoEntero_error(p):
+    'tipoEntero : TINT EQUAL error'
+    cola.append("Error Semántico en la asignación de variable tipo INT. Valor no válido.")
+
+def p_tipoLong(p):
+    'tipoLong : TLONG EQUAL LONG'
+
+def p_tipoLong_error(p):
+    'tipoLong : TLONG EQUAL error'
+    cola.append("Error Semántico en la asignación de variable tipo LONG. Valor no válido.")
+
+def p_tipoFloat(p):
+    'tipoFloat : TFLOAT EQUAL FLOAT'
+
+def p_tipoFloat_error(p):
+    'tipoFloat : TFLOAT EQUAL error'
+    cola.append("Error Semántico en la asignación de variable tipo FLOAT. Valor no válido.")
+
+def p_tipoBoolean(p):
+    'tipoBoolean : TBOOLEAN EQUAL BOOLEAN'
+
+def p_tipoBoolean_error(p):
+    'tipoBoolean : TBOOLEAN EQUAL error'
+    cola.append("Error Semántico en la asignación de variable tipo BOOLEAN. Valor no válido.")
+
+def p_tipoChar(p):
+    'tipoChar : TCHAR EQUAL CHAR'
+
+def p_tipoChar_error(p):
+    'tipoChar : TCHAR EQUAL error'
+    cola.append("Error Semántico en la asignación de variable tipo CHAR. Valor no válido.")
+
+def p_tipoString(p):
+    'tipoString : TSTRING EQUAL STRING_1'
+
+def p_tipoString_error(p):
+    'tipoString : TSTRING EQUAL error'
+    cola.append("Error Semántico en la asignación de variable tipo STRING. Valor no válido.")
 
 def p_tipoDato(p):
     '''tipoDato : TINT
